f2595bd5719383562d4f44b49140006ed0f06896
bishalkhatri06 <bishalkhatri52@gmail.com>
2023-06-05 16:15:01 +0545

    first commit

---
+++ src/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <>
+        <h1>This is a first component</h1>
+        <h1>We are using functional component</h1>
+    </>
+  )
+}
+
+export default First
---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Test = () => {
+    return (
+        <>
+            <h2>This is not a default export component</h2>
+        </>
+    )
+}
+
+export const Show = () => {
+    return (
+        <>
+            <h1>Welcome to React Component</h1>
+        </>
+    )
+}
